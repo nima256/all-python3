2fe8ebbe49ca70f54f15d38506c5be4e6e5c1820
nimamahdi <nimamahdi1385@gmail.com>
2021-07-05 16:22:39 +0430

    if

---
+++ if.py
@@ -0,0 +1,8 @@
+age = int(input(" its simple game...please enter a number between 1 to 10======>"))
+
+if age == 4:
+    print("you win")
+elif age < 5:
+    print("you lose")
+elif age > 5:
+    print("you say close but you lose")
