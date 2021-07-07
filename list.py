6fa166f29f03d9513ea7e550a3094fe1225c0b30
nimamahdi <nimamahdi1385@gmail.com>
2021-07-07 19:19:28 +0430

    list

---
+++ list.py
@@ -0,0 +1,18 @@
+# we make list this like:
+list = [2 , 4 , 1 , 3]
+
+#now we want to order the list
+list.sort()
+# output [1 , 2 , 3 , 4]
+
+# here i eant to add a number to my list
+list.append(5)
+# this add 5 to end of the list
+
+# now i want to reverse the list
+list.reverse()
+# output [3 , 1 , 4 , 2]
+
+# here i want to remove a number or every thing that you write it in list
+list.remove(1)
+
