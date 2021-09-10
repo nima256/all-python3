e14767f7806fe41ee782fd158258973aa9e072f8
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-11 03:43:06 +0430

    if,elif,else

---
+++ all_python3_3.py
@@ -0,0 +1,28 @@
+# today we are going to talk about if
+# what is if?
+# if is condition that you can write
+# how write it?
+# we write "if" and after that we have to give a variable for example:
+# if number == 2:
+    #print(you win)
+# what is elif?
+# "elif" is another condition that it always write after "if"
+# and what is else?
+# you write else when that you want to say if all the up condition was false print this
+# lets see an example
+age = int(input(" its simple game...please enter a number between 1 to 100======>"))
+
+if age == 37:
+    print("you win")
+elif age < 37:
+    print("unfotunately you lose,because you had to say higher ")
+elif age  == 50:
+    print("unfotunately you lose,because you had to say lower")
+# what is else?
+# else is condition that we=hen all thing was wrong it say what to do
+# how write it?
+# we write "else"and ":" in front of it and give
+
+# space to it and write every thing we want
+else :
+    print("you lose")
