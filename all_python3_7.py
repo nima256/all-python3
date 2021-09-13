b8f21479a9ecda22395135142d6e8b00f0447e50
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-13 13:12:57 +0430

    something easy

---
+++ all_python3_7.py
@@ -0,0 +1,23 @@
+# hello 
+# i hope you are good 
+# this script is something easy that are very usefull
+
+# it takes you exponent number between 1 to 100
+a = [x**2 for x in range(1 , 100)]
+print(a)
+
+#it takes you odd numbers between 1 to 100
+for x in range(1 , 100 , 2):
+    print(x)
+
+
+# it takes you minimum and maximum and sum list to you
+a = [3 , 1 , 2 , 5 , 4 , 8 , 7 , 6 , 10 , 9]
+print(min(a))
+print(max(a))
+print(sum(a))
+
+
+#it print for you a number that you want of the list 
+a = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+print (a[ : 3])
