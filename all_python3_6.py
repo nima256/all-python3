26bbcba4f79d7b35c844888b005df77deb314816
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-13 13:08:29 +0430

    dictionary

---
+++ all_python3_6.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+# hello here we want to talk about dictionary
+# how to creat dictionary
+# first we have to enter a name for it and creat variable
+# second we write dictionary with "{}" this
+# third we have to enter name of our dictionary and enter a dictionart
+# e.g
+dic = {'name' : 'nima' , 'lstname' : 'mahdi' , 'age' : '15'}
+print(dic)
+# output == {'name': 'nima', 'lstname': 'mahdi', 'age': '15'}
+
+
+# also we can print dictionary that we want 
+# e.g
+dic2 = {'name' : 'nima' , 'lstname' : 'mahdi' , 'age' : '15'}
+# in print after write variable name and when we open [] this.. 
+# ..systen automatic enter dictionary key name and you can choose it  
+print(dic['lstname'])
+
+# dictionary is easy to learn
+# so and this script is short
+
+
+# thanks for reading (:
