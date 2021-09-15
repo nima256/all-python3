06b39e4187454678ed88ee0e5dad6260d93cc00b
nimamahdi <www.nimamahdi1385@gmail.com>
2021-09-15 12:48:10 +0430

    def

---
+++ all_python3_8.py
@@ -0,0 +1,41 @@
+# hello 
+# i hoe you are good 
+# here i want to talk about "def"
+
+# what is def?
+# def is command that you writ every thing you want in it and.... 
+# ...at the end if you want to run it you  have to call def name
+# for example this def name is a
+# for run it you just have to write def name for example ==> python0()
+
+def python0():
+    print("hello world")
+
+# in this def when you want to print it you must to write a something..
+#.. in the braces 
+# in your print you can use thing that user write
+# for run this def you have to do this ==> python1('nima')
+def python1(a):
+    print(f'my name is {a}')
+
+# in this def when you want to print it you must to write two something..
+# .. or how many times do you want
+# here i use \n and \t what are this?
+# \n takes you 
+# to next line 
+# \t give tab   to your print 
+def python2(a , b):
+    print(f'my name is {a} \nand  \tmy age is {b} ')
+
+# in this def we want to return def
+# what is return?
+# return dose not print for you it saved for you
+# if you want to print it you have to saved your def in the variable
+def python3(a , b):
+    c = a+b
+    return c 
+test = python3(2 , 3)
+print(test)
+
+# i hope it will help you (:  
+
