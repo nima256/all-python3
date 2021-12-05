c04936e0d8e95f5c8cabcd64e6d76a66e3a402fc
nimamahdi <83651805+nimamahdi@users.noreply.github.com>
2021-12-05 11:40:43 +0330

    math with python

--- all_python3_02.py
+++ all_python3_02.py
@@ -1,14 +1,20 @@
 # math with python (:
 
+# you can print ' addition' of number 
 print(18+15)
 # output ==> 33
 
+# subtraction of number
+print(18-15)
+
+# multiplication of number
 print(2*4)
 # output ==> 8
 
+# division of number
 print(4/2)
 # output ==> 2
 
-
+# exponent of number
 print(2**2)
 # output ==> 4
