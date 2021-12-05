d223cf94a76709ff7a2351d1660f70a5e5f616f6
nimamahdi <83651805+nimamahdi@users.noreply.github.com>
2021-12-05 11:30:24 +0330

    variable

--- all_python3_01.py
+++ all_python3_01.py
@@ -2,12 +2,41 @@
 # a variable is something you can save something in it
 # if the amounts is word you have to take it in "" double quotation 
 # if the amounts is number you should not take double quotation
-# for example
+
+# valid names for variavle:
+# e.x:
+# data_2021_12_5 = ""
+# RESULT_1 = ""
+# Result_1 = ""
+# ouTput = ""
+# _Output = ""
 name = "nima"
-# in here for last name we can not use space   we have to use _ or evey thing that you want
-last_name = "mahdi"
-age = 14
+laSt_nAme = "mahdi"
+_Age = 14
+
+
+# invalid names for varible:
+# for all variable we can not use space  ' ', a varible that start with number '1_hello'
+# we can not use ' this character "john's_salary" 
+# we can not use hyphen 'hello-there' 
+# we can not use $ this
+# character 'price_in_$'
+
+# last name = "mahdi"
+# output ==> SyntaxError: invalid syntax
+
+# 1_person = "john"
+# output ==> SyntaxError: invalid decimal literal
+
+# john's_salary = 25000000
+# output ==> SyntaxError: EOL while scanning string literal
+
+# hello-there = 'hello'
+# output = SyntaxError: cannot assign to operator
+
+# price_in_$ = '$'
+# output ==> SyntaxError: invalid syntax
 
 # for print that we just have to print name of the variable
-print(age)
+print(_Age)
 # output ==> 14
